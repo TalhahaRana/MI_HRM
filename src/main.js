1764c10273a396b230be5041dc505d1baaa4771d
Arham-Imdad <arham.imdad@staxk.com>
2024-10-08 17:17:53 +0500

    rewrite main.js

--- src/main.js
+++ src/main.js
@@ -3,27 +3,24 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'; // You can import any icon you need
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons"; // Import any icon you need
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css"; // Import Bootstrap Icons
 
-const app = createApp(App)
-
-
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
-import "bootstrap"; // Import Bootstrap JS (optional)
-
+// Add FontAwesome icons to the library
 library.add(faUserSecret);
 
-app.component('font-awesome-icon', FontAwesomeIcon);
-app.use(router)
-
-
+// Create the Vue application
 const app = createApp(App);
 
+// Register the FontAwesomeIcon component
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+// Use the router
 app.use(router);
 
+// Mount the application
 app.mount("#app");
