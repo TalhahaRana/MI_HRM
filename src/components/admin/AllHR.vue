1ce3fa8c8c2b8d0f34c9de0d2aea09ec3f23188d
Samia Saghir <SamiaSaghir89@gmail.com>
2024-10-08 16:51:10 +0500

    admin task done

---
+++ src/components/admin/AllHR.vue
@@ -0,0 +1,82 @@
+
+//all hr
+<template>
+    <div class="table-container">
+      <h2>All HR List</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+        
+          <tr>
+            <td>Samia</td>
+            <td>samia@gmail.com</td>
+            <td>HR</td>
+            <td>
+              <button @click="editItem">Edit</button>
+              <button @click="deleteItem">Delete</button>
+            </td>
+          </tr>
+      
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HRTable',
+    methods: {
+      editItem() {
+        
+        console.log("Edit button clicked");
+      },
+      deleteItem() {
+  
+        console.log("Delete button clicked");
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .table-container {
+    padding: 20px;
+  }
+  
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 20px;
+  }
+  
+  th, td {
+    border: 1px solid #ddd;
+    padding: 12px;
+    text-align: left;
+  }
+  
+  th {
+    background-color: #f2f2f2;
+    font-weight: bold;
+  }
+  
+  button {
+    margin-right: 10px;
+    padding: 5px 10px;
+    background-color:var(--basic-button);
+    color: white;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  
+
+  </style>
+  
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,10 @@ import Login from "../views/auth/Login.vue";
 import ForgotPass from "@/views/auth/ForgotPass.vue";
 import LeaveApplication from "@/components/employee/leaveApplication.vue";
 import CheckInOut from "@/components/employee/CheckInOut.vue"
+import AddHR from "@/components/admin/AddHR.vue";
+import AllHR from "@/components/admin/AllHR.vue";
+import Projects from "@/components/admin/Projects.vue";
+import CheckInCheckOut from "@/components/admin/CheckInCheckOut.vue";
 
 
 const router = createRouter({
@@ -13,6 +17,7 @@ const router = createRouter({
       name: "login",
       component: Login,
     },
+    {
       path: '/check',
       name: 'CheckInOut',
       component: CheckInOut
@@ -26,6 +31,22 @@ const router = createRouter({
       name: "leaveApplication",
       component: LeaveApplication,
     },
+    {
+    path:"/Add-HR",
+    name: "AddHR",
+    component:AddHR
+    },
+    {
+      path:"/All-HR",
+      name:"AllHR",
+      component:AllHR
+    },
+    {
+      path:"/project",
+      name:"Projects",
+      component:Projects
+    },
+   
   ]
 })
 export default router
