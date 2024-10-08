b1a0291fd6e668f7c9e4c39f13d5a3bfe5958dd7
Arham-Imdad <arham.imdad@staxk.com>
2024-10-08 16:34:57 +0500

    Add Assignedproject and AttendanceEmployee

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import router from "./router";
 
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 import "bootstrap"; // Import Bootstrap JS (optional)
 
 const app = createApp(App);
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../views/auth/Login.vue'
-import ForgotPass from '@/views/auth/ForgotPass.vue'
-import CheckInOut from "@/components/employee/CheckInOut.vue"
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "../views/auth/Login.vue";
+import ForgotPass from "@/views/auth/ForgotPass.vue";
+import CheckInOut from "@/components/employee/CheckInOut.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'login',
-      component: Login
+      path: "/",
+      name: "login",
+      component: Login,
     },
     {
-      path: '/check',
-      name: 'CheckInOut',
-      component: CheckInOut
+      path: "/check",
+      name: "CheckInOut",
+      component: CheckInOut,
     },
     {
-      path:'/forgot-pass',
-      component:ForgotPass
-    }
-  ]
-})
+      path: "/forgot-pass",
+      component: ForgotPass,
+    },
+  ],
+});
 
-export default router
-
-
+export default router;
