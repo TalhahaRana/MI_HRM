276e0074c85091d64b50fd6e1a01710e2f73ce45
Choudrytalha420 <tasif6094@gmail.com>
2024-10-08 12:34:32 +0500

    leave Application

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../views/auth/Login.vue'
-import ForgotPass from '@/views/auth/ForgotPass.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Login from "../views/auth/Login.vue";
+import ForgotPass from "@/views/auth/ForgotPass.vue";
+import LeaveApplication from "@/components/employee/leaveApplication.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'login',
-      component: Login
+      path: "/",
+      name: "login",
+      component: Login,
     },
     {
-      path:'/forgot-pass',
-      component:ForgotPass
-    }
-  ]
-})
+      path: "/forgot-pass",
+      component: ForgotPass,
+    },
+    {
+      path: "/leaveApplication",
+      name: "leaveApplication",
+      component: LeaveApplication,
+    },
+  ],
+});
 
-export default router
-
-
+export default router;
